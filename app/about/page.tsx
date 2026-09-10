b318934f5d0212af77363b5581f1db66832320b4
Mehakpixxelu <bhardwajmehakpixxelu@gmail.com>
2026-09-10 16:47:07 +0530

    Add About Us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,8 @@
 export default function AboutPage() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        padding: "80px 40px",
-        background: "#111",
-        color: "#fff",
-      }}
-    >
+    <main>
       <h1>About Us</h1>
-
-      <p>
-        This page is created in Next.js.
-      </p>
-
-      <p>
-        If you can see this page, Next.js routing is working.
-      </p>
+      <p>This About Us page is created in Next.js.</p>
     </main>
   );
 }
